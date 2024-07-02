4006183fc65cd66293ce0340849a7b9ca2a91523
rytjr <kks007159@naver.com>
2024-07-02 13:56:23 +0900

    math1

---
+++ src/change/change.java
@@ -0,0 +1,16 @@
+package change;
+
+import java.util.*;
+
+public class change {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		int num = sc.nextInt();
+		
+		System.out.print(Integer.parseInt(str,num));
+	}
+}
